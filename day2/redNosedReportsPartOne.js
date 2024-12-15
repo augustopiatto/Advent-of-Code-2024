cc6fd6687835eb508fa639036d69675b04d1d5f1
augustopiatto <ampiatto@hotmail.com>
2024-12-15 14:44:18 -0300

    feat(day 2): segunda parte

--- day2/redNosedReportsPartOne.js
+++ day2/redNosedReportsPartOne.js
@@ -1,27 +1,30 @@
 import { sanitizeData } from "./sanitizeData.js";
 
 function checkIfReportIsSafe(report) {
-  let type = null;
+  const isOrdered = (arr, type) =>
+    arr.every((num, index) => {
+      if (!arr[index + 1]) return true;
+      const diff = arr[index + 1] - num;
+      if (type === "increasing") return diff > 0 && diff <= 3;
+      if (type === "decreasing") return diff < 0 && diff >= -3;
+      return false;
+    });
 
-  const isSafe = report.every((number, index) => {
-    if (report[index + 1]) {
-      if (number > report[index + 1]) {
-        if (type === "increasing" || number - report[index + 1] > 3)
-          return false;
-        type = "decreasing";
-      } else if (number < report[index + 1]) {
-        if (type === "decreasing" || report[index + 1] - number > 3) {
-          return false;
-        }
-        type = "increasing";
-      } else {
-        return false;
-      }
-    }
+  if (isOrdered(report, "increasing") || isOrdered(report, "decreasing")) {
     return true;
-  });
+  }
 
-  return isSafe;
+  for (let i = 0; i < report.length; i++) {
+    const modifiedReport = report.slice(0, i).concat(report.slice(i + 1));
+    if (
+      isOrdered(modifiedReport, "increasing") ||
+      isOrdered(modifiedReport, "decreasing")
+    ) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function getSafeReports() {
